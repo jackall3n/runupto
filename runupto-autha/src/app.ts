bf6944c52ffcf4f403ddae955215da2caa392bec
Jack Allen <jackallensemail+git@gmail.com>
2019-12-22 10:08:13 +0000

    test

--- runupto-autha/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-export interface App extends express.Application {
-  locals: {}
-}
-
-export const app: App = express();
-
-export default app;
--- runupto-autha/src/utils/applyMiddleware.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-export default (app: express.Application | any, middleware: any[]) => {
-  for (const m of middleware) {
-    app.use(m);
-  }
-}
